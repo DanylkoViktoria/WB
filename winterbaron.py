aea5d915231998d8cd6080e9ac67b10f9df00019
DanylkoViktoria <135425305+DanylkoViktoria@users.noreply.github.com>
2025-09-28 20:56:03 +0300

    Add files via upload

--- winterbaron.py
+++ winterbaron.py
@@ -4,28 +4,33 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
-#  Змінні середовища
+# 🔑 Змінні середовища (Railway або локально)
 TOKEN = os.getenv("TOKEN") or "8422502818:AAE3iEbsck7e67HmJKVsHRMFvtnShFahbxQ"
 GROUP_ID = int(os.getenv("GROUP_ID") or -4867326536)
 
+# Словник для збереження відповідності повідомлень групи <-> користувач
 bridge = {}
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# 📌 Команда /start
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer("Бот для тейков💥💥")
 
+# 📌 Користувач пише у приват
 @dp.message(lambda msg: msg.chat.type == "private")
 async def user_message(message: Message):
     sent = await bot.send_message(
         GROUP_ID,
-        f"Від {message.from_user.full_name} (id:{message.from_user.id}):\n{message.text}"
+        f" {message.from_user.full_name} (id:{message.from_user.id}):\n{message.text}"
     )
+    # Прив’язуємо повідомлення у групі до користувача
     bridge[sent.message_id] = message.from_user.id
-    await message.answer("✅ Сообщение отправлено")
+    #await message.answer("✅ Повідомлення переслано адміну")
 
+# 📌 Адмін відповідає у групі на повідомлення користувача
 @dp.message(lambda msg: msg.chat.id == GROUP_ID and msg.reply_to_message)
 async def group_reply(message: Message):
     replied_id = message.reply_to_message.message_id
@@ -35,7 +40,10 @@ async def group_reply(message: Message):
             user_id,
             f"{message.text}"
         )
+    else:
+        await message.reply("⚠ Не знайдено користувача для цієї відповіді.")
 
+# 📌 Запуск
 async def main():
     await dp.start_polling(bot)
 
@@ -47,9 +55,8 @@ if __name__ == "__main__":
         loop = None
 
     if loop and loop.is_running():
-        # Якщо loop вже працює (Spyder/Jupyter)
         import nest_asyncio
-        nest_asyncio.apply()      # дозволяє запускати корутини всередині вже працюючого loop
+        nest_asyncio.apply()
         asyncio.create_task(main())
     else:
         asyncio.run(main())
